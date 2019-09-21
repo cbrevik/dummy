5d1dd06d4a0d367d8534d6b5fca8fc10d7cf22d9
Christian Brevik <christian.brevik@gmail.com>
2019-09-21 19:46:42 +0200

    Create spiderman.js

---
+++ spiderman.js
@@ -0,0 +1,20 @@
+const hello = () => {
+  // nope
+  return    true;
+}
+
+
+hello();
+
+console.log("YEP" )
+
+
+
+// new comment
+
+let i = 1; i++; //stop
+
+for (let j = 2; i < 10; j++) 
+{
+  console.log("Hey")
+}
